24a84f640d88c2877306502062eb73ee203e3d80
Ponce Facundo <FacundoIgnacioPonce@alu.frt.utn.edu.ar>
2026-05-01 19:11:13 -0300

    Problema 2 resuelto

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta (long productCode, string productDescription, int quantity, decimal unitPrice) 
+        {
+            decimal totalCalculado = 0;
+
+            if (quantity > 0)
+            {
+                totalCalculado = quantity * unitPrice;
+            }
+
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = totalCalculado,
+            };
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+
+        }
+
+    }
+}
