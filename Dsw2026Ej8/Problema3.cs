26e4bcb12d56bd357b83679f47f8514fe9b51547
Ponce Facundo <FacundoIgnacioPonce@alu.frt.utn.edu.ar>
2026-05-01 19:34:05 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias (int originalValue, Product product) 
+        {
+            int copiaValor = originalValue;
+            copiaValor++;
+
+            Product copiaProducto = product;
+
+            copiaProducto.SetDescription("Nueva Descripcion");
+
+            return $"{originalValue}-{copiaValor}-{product.GetDescription()}";
+        }
+    }
+
+    public class Product
+    {
+        private string description = "";
+
+        public string GetDescription() 
+        { 
+            return description;
+        }
+
+        public void SetDescription(string nuevaDescription) 
+        {
+            description = nuevaDescription;
+        }
+    }
+}
